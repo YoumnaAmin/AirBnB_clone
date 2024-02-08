ae3893310a3693457121a48fdcfe3053cbe8abe0
YoumnaAmin <youmnamin564@gmail.com>
2024-02-09 00:21:15 +0300

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -6,30 +6,38 @@ import json
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """Base model for all classes"""
 
-    def __init__(self):
-        #for key, value in kwargs.items():
-           # if key == "created_at" or key == "updated_at":
-               # value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-           # setattr(self, key, value)
-        #if "id" not in kwargs.keys():
+    def __init__(self, *args, **kwargs):
+        """Constructor method"""
+
+        for key, value in kwargs.items():
+            if key == "__class__":
+                continue
+            if key == "created_at" or key == "updated_at":
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            setattr(self, key, value)
+        if "id" not in kwargs.keys():
             self.id = str(uuid.uuid4())
-       # if "created_at" not in kwargs.keys():
+        if "created_at" not in kwargs.keys():
             self.created_at = datetime.now()
-        #if "updated_at" not in kwargs.keys():
+        if "updated_at" not in kwargs.keys():
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """to print in str format"""
         return "[{:s}] ({:s}) {:s}".format(
             self.__class__.__name__, self.id, str(self.__dict__)
         )
-    
+
     def save(self):
+        """to update the creation time"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """to print in dictionary format"""
         _copy = self.__dict__.copy()
         _copy["__class__"] = self.__class__.__name__
         _copy["created_at"] = self.created_at.isoformat()
