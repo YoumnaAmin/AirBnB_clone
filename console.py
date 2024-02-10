39cec0d7d1bd4f3b08f9a3e7c4f4ce521124916a
YoumnaAmin <youmnamin564@gmail.com>
2024-02-11 01:26:31 +0300

    ..

---
+++ console.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A console to manipulate the objects"""
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """a cmd console class that inherits from cmd class"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg: str):
+        """Quit command to exit the program
+        """
+        return True
+
+    def do_eof(self, arg):
+        """Simulate an EOF condition.
+        """
+        return True
+
+    def do_help(self, arg: str):
+        """to get help
+        """
+        return super().do_help(arg)
+
+    def emptyline(self):
+        """empty line doesn't affect anything
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
