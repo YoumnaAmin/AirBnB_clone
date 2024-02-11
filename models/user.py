342b06b44d6f5f54c84713d2cdad74e1ab39eca4
YoumnaAmin <youmnamin564@gmail.com>
2024-02-11 20:46:53 +0300

    ..

--- models/user.py
+++ models/user.py
@@ -2,10 +2,12 @@
 """User Module"""
 
 
-class User:
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
     """User Class"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-    
